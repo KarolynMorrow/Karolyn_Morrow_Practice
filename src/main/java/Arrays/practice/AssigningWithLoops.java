d3c1169e05bb0d5434c0e441fe4c413e4efbb36d
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-03-27 16:10:12 -0400

    Finished fifth exercise

--- src/main/java/Arrays/practice/AssigningWithLoops.java
+++ src/main/java/Arrays/practice/AssigningWithLoops.java
@@ -2,6 +2,11 @@ package Arrays.practice;
 
 public class AssigningWithLoops {
     public static void main(String[] args) {
+        int[] emptyArr = new int[5];
+        for (int i = 0; i < emptyArr.length; i++){
+            emptyArr[i] = i;
+            System.out.println(emptyArr[i]);
+        }
 
     }
 }
