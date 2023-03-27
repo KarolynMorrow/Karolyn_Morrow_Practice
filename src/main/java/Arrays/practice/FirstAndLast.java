7bb63206b3cd83cfdb59e284e5ac03f0bee2bbd7
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-03-27 16:06:28 -0400

    Finished fourth exercise

--- src/main/java/Arrays/practice/FirstAndLast.java
+++ src/main/java/Arrays/practice/FirstAndLast.java
@@ -2,6 +2,17 @@ package Arrays.practice;
 
 public class FirstAndLast {
     public static void main(String[] args) {
+       int[] arrayInt = {55, 27, 32, 56, 88};
+        System.out.println(arrayInt[0]);
+        System.out.println(arrayInt[4]);
+
+        for(int i = arrayInt.length - 1; i < arrayInt.length; i++){
+            System.out.println(arrayInt[i]);
+        }
+
+        for(int i = 0; i < arrayInt.length; i++){
+            System.out.println(arrayInt[i]);
+        }
 
     }
 }
