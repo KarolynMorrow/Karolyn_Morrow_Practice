a39ba717763701811c97c8ad92c66374348e7a9a
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-03-29 11:00:32 -0400

    Updated array words in homework

--- src/main/java/Arrays/practice/Main8Homework.java
+++ src/main/java/Arrays/practice/Main8Homework.java
@@ -15,12 +15,13 @@ public class Main8Homework {
         String sentence = input.nextLine(); //Assigning the input to sentence
         int sentenceLength = sentence.length(); //taking the length of the input
 
-        String[] words = new String[sentenceLength]; //making an array based on the length of input
-        //System.out.println(Arrays.toString(words)); //Print array words as a string
+        String[] words = sentence.split(" "); // Split the string into an array
+        //String[] words = new String[sentenceLength]; //making an array based on the length of input
+
 
 
         for (int i = 0; i < sentenceLength; i++) { //start at int = 0 , if index is less than the length of sentence then continue
-           words = sentence.split(" ,"); //split sentence and place into array words
+            System.out.println(Arrays.toString(words));
         }
         System.out.println(Arrays.toString(words)); //print array words as a string
 
