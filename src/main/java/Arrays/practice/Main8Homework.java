db2d98f8bc275efbee7256f3c09309968ba5a743
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-03-30 03:46:20 -0400

    Updated homework with comments on what to do next

--- src/main/java/Arrays/practice/Main8Homework.java
+++ src/main/java/Arrays/practice/Main8Homework.java
@@ -13,48 +13,67 @@ public class Main8Homework {
         Scanner input = new Scanner(System.in);
         System.out.println("What sentence would you like to count?");
 
-        String sentence = input.nextLine().replace(".", " "); //Assigning the input to sentence and taking out the period
+        String sentence = input.nextLine().replace(".", ""); //Assigning the input to sentence and taking out the period
         int sentenceLength = sentence.length(); //taking the length of the input
 
 
         String[] words = sentence.split(" "); // Split the string into an array
         //String[] words = new String[sentenceLength]; //making an array based on the length of input
-        int arrayLength = words.length;
-        String[] cloneWords = new String[arrayLength]; //makes length of cloneWords array
+        int wordsArrayLength = words.length;
+        String[] uniqueWords;
+        uniqueWords = words.clone(); //Clones array words
+        Arrays.sort(uniqueWords); //sorted uniqueWords array so duplicates are beside one another
+        //find a way to only take words that are duplicated and put only one instance of them into array uniqueWords
+
+
+
+        System.out.println(Arrays.toString(uniqueWords)); //print array uniqueWords as a string
+
 
         System.out.println(" ");
-
-
-        System.out.println(Arrays.toString(words)); //print array words as a string
-        System.out.println(Arrays.toString(cloneWords));
-
-        System.out.println(" ");
-
 
 
         for (int i = 0; i < sentenceLength; i++) { //Moving through the array start at int = 0 , if index is less than the length of sentence then continue
             int counter = 0;
-             for (int j = 0; j < arrayLength; j++) { //loop through array and add counting each occurrence
-                 cloneWords = words.clone();
-                if (words[i].equals(cloneWords[j])) { //if index in words array equals index in cloneWords array
+            //System.out.println();
+            for (int j = 0; j < wordsArrayLength ; j++) { //loop through array and add counting each occurrence
+                if (words[i].equals(uniqueWords[j])) { //if index in words array equals index in uniqueWords array
+
                     counter++; //add how many times it shows
-                    cloneWords[i] = words[i];
-                    System.out.println(Arrays.toString(cloneWords));
+
+                    //System.out.println();
+                    //uniqueWords[i] = words[i];
+                    //System.out.println(Arrays.toString(uniqueWords)); //Make sure uniqueWords isn't showing values of null
 
                 }
+                //System.out.println();
 
-                }
-            System.out.println(cloneWords[i] + " shows up: " + counter + " times.");
+            }
+
+            System.out.println(uniqueWords[i] + " shows up: " + counter + " times.");
+
         }
 
 
     }
 
 
-    public static void marchTwentyNinthHW(){
-
-
-        /*https://www.hackerrank.com/challenges/java-negative-subarray/problem*/
+    public static void marchTwentyNinthOptionalHW() {
+        /*Binary search method*/
+        int[] myIntArray = {5, 6, 2, 10, 20, 40};
+        boolean searchResult = binarySearch(myIntArray, 6);
+        System.out.println(searchResult);
     }
 
+    private static boolean binarySearch(int[] myIntArray, int targetNumber) {
+        // TODO - write your code here
+
+        return false;
+
+    }
 }
+
+/*https://www.hackerrank.com/challenges/java-negative-subarray/problem*/
+
+
+
